d80a749896a2219f8f398c939c997846d9774b71
Paras Varshney <paras.varshney97@gmail.com>
2020-04-30 17:52:08 +0530

    build model and build test images

--- app.py
+++ app.py
@@ -7,3 +7,47 @@ Created on Thu Apr 30 14:22:15 2020
 
 import pandas as pd
 import numpy as np
+from sklearn.externals import joblib 
+
+from flask import Flask, jsonify, request
+import json
+import flask
+
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    form_data = request.form.to_dict()
+    print("Uploaded image path: "+str(form_data["input_image"]))
+
+    test_image = image.load_img(form_data["input_image"], target_size = (32, 32))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    print(test_image)
+
+    classifier = joblib.load('prediction_classifier.pkl')
+    result = classifier.predict(test_image)
+
+    if result[0][0] == 1:
+        prediction = 'Dog'
+    else:
+        prediction = 'Cat'    
+    return flask.render_template('index.html', predicted_value="Uploaded image was of:\n {}".format(prediction))
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
--- model.py
+++ model.py
@@ -18,40 +18,51 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 
-def data_preprocessing():
-    # Images preprocessing before training
-    train_datagen = ImageDataGenerator(rescale = 1./255,
-                                       shear_range = 0.2,
-                                       zoom_range = 0.2,
-                                       horizontal_flip = True)
-    
-    test_datagen = ImageDataGenerator(rescale = 1./255)
-    
-    training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                     target_size = (32, 32),
-                                                     batch_size = 32,
-                                                     class_mode = 'binary')
-    
-    test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                                target_size = (32, 32),
-                                                batch_size = 32,
-                                                class_mode = 'binary')
-    
+# Images preprocessing before training
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
 
+test_datagen = ImageDataGenerator(rescale = 1./255)
 
-def build_model():
-    classifier = Sequential()
-    classifier.add(Conv2D(32, 3, 3, input_shape=(32, 32, 3), activation="relu"))
-    classifier.add(MaxPooling2D(pool_size=(2, 2)))
-    classifier.add(Flatten())
-    classifier.add(Dense(units= 128, activation="relu"))
-    classifier.add(Dense(units = 1, activation = 'sigmoid'))
-    classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
 
 
 
-if __name__ == '__main__':
-    data_preprocessing()
-    build_model()
-    classifier.fit_generator(training_set, steps_per_epoch = 8000, epochs = 2, validation_data = test_set, validation_steps = 2000)
-    joblib.dump(classifier, 'prediction_classifier.pkl') 
+
+classifier = Sequential()
+classifier.add(Conv2D(32, 3, 3, input_shape=(64, 64, 3), activation="relu"))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Conv2D(32, 3, 3, activation="relu"))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(units= 128, activation="relu"))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+classifier.fit_generator(training_set, steps_per_epoch = 8000, epochs = 5, validation_data = test_set, validation_steps = 2000)
+print(joblib.dump(classifier, 'prediction_classifier.pkl'))
+
+
+
+
+# testing on a single input
+test_image = image.load_img("./test_image_1.jpg", target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+print(test_image)
+result = classifier.predict(test_image)
+
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat' 
+print(prediction)
