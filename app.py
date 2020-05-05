6ce53f66569c09af7841544a4e0ab3180df233a3
Paras Varshney <paras.varshney97@gmail.com>
2020-05-05 15:36:43 +0530

    working on uploading files

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 
 
+from io import BytesIO
+import urllib
+
+
+def loaderImage(URL):
+    with urllib.request.urlopen(URL) as url:
+        img = image.load_img(BytesIO(url.read()), target_size=(125, 125))
+
+    return image.img_to_array(img)
+
+
 app = Flask(__name__)
 
 IMAGE_SIZE = 64
@@ -29,12 +40,17 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     form_data = request.form.to_dict()
-    print("Uploaded image path: "+str(form_data["input_image"]))
+    print(form_data)
+    print("Uploaded image path: "+str(form_data["image_path"]))
 
-    test_image = image.load_img(form_data["input_image"], target_size = (IMAGE_SIZE, IMAGE_SIZE))
-    test_image = image.img_to_array(test_image)
+    #test_image = image.load_img(form_data["image_path"], target_size = (IMAGE_SIZE, IMAGE_SIZE))
+    #test_image = image.img_to_array(test_image)
+    
+    test_image = loaderImage(form_data["image_path"][5:])
+    
     test_image = np.expand_dims(test_image, axis = 0)
     print(test_image)
+    
 
     classifier = joblib.load('prediction_classifier.pkl')
     result = classifier.predict(test_image)
